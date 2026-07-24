79d2a919e5809e11e0a74b7b9d7b9f55e4019e9a
Pravin Prajapati <prajapatip2012006@gmail.com>
2026-07-24 13:26:04 +0100

    commited

--- src/main/java/com/rest/RestAPis/service/EmailService.java
+++ src/main/java/com/rest/RestAPis/service/EmailService.java
@@ -8,7 +8,9 @@ package com.rest.RestAPis.service;
  *
  * @author Pravin Prajapati
  */
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -19,6 +21,25 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    
+    @Value("${spring.mail.host}")
+private String host;
+
+@Value("${spring.mail.port}")
+private String port;
+
+@Value("${spring.mail.username}")
+private String username;
+
+@PostConstruct
+public void checkMailConfig() {
+    System.out.println("HOST = " + host);
+    System.out.println("PORT = " + port);
+    System.out.println("USERNAME = " + username);
+}
+    
+    
+    
    public void sendOtp(String toEmail, String otp) {
 
     try {
